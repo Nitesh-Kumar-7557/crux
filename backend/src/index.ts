3ba3cf258f55cc36b58ad2ea033304c71bc360db
Nitesh Kumar <master@Niteshs-MacBook-Pro.local>
2026-04-05 18:28:53 +0530

    fix: resolve deployment issues for stable v0.1.0 release

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,5 +2,5 @@ import app from "./app.js";
 import config from "./config/index.js";
 
 app.listen(config.server_port, ()=> {
-    console.log("Server is up and running...")
+    console.log(`Server is up and running on ${config.server_port}...`)
 })
