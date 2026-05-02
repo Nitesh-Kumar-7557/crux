91514184df555e3b09d7e62a209a0dc1cb4c2ddf
Aditya Singla <exploringhobby@gmail.com>
2026-05-02 12:52:14 +0530

    feat: moved 'like' logic validation from frontend to backend using auth validation

--- backend/src/routes/like.route.ts
+++ backend/src/routes/like.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { registerLike } from "../controllers/like.controller.js";
+import { authMiddleware } from "../middlewares/auth.js";
 
 const likeRoutes = Router();
 
-likeRoutes.post("/", registerLike);
+likeRoutes.post("/", authMiddleware, registerLike);
 
 export default likeRoutes;
